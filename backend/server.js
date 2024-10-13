86bc05f90d4aab349ac13d2634d54c66a16a85a7
Alejandro <acervantest83@gmail.com>
2024-10-13 15:55:19 -0700

    DB connection, first document and POST  endpoint implementation

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,37 @@
-import express from 'express';
+import express from 'express'
+import dotenv from 'dotenv'
+import { connectDB } from './config/db.js'
+import Product from './models/product.model.js'
+
+dotenv.config()
 
 const app = express();
 
+app.use(express.json()) // allows us to accept JSON data in the req.body
+
 app.get("/", (req, res) => {
-    res.send("Server is ready")
+    res.send("Server ready!!!") 
+})
+
+app.post("/products", async (req, res) => {
+    const product = req.body // user will send this data
+
+    if(!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success:false, message:"Please provide all fields" })
+    }
+
+    const newProduct = new Product(product)
+
+    try {
+        await newProduct.save()
+        res.status(201).json({ success:true, data:newProduct })
+    } catch (error) {
+        console.log("Error in create product: ", error.message)
+        res.status(500).json({ success:false, message:"Server error" })
+    }
 })
 
 app.listen(3000, () => {
+    connectDB()
     console.log('Server started at http://localhost:3000')
 })
