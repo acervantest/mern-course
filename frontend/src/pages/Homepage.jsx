33461b91c7a18f5f5a3e0159c9c5fd3999de0fad
Alejandro <acervantest83@gmail.com>
2024-10-23 08:13:24 -0700

    Home page no products

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { Container, VStack, Text } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <Container maxW='container.xl' py={12}>
+      <VStack spacing={8}>
+        <Text
+          fontSize={'30'}
+          fontWeight={'bold'}
+          bgGradient={'linear(to-r), cyan.400, blue.500'}
+          textAlign={'center'}
+        >
+          Current Products
+        </Text>
+
+        <Text>
+          No products found - { ' ' }
+          <Link to={'/create'}>
+            <Text as={'span'} color={'blue.500'} _hover={{ textDecoration: 'underline' }}>
+              Create Product
+            </Text>
+          </Link>
+        </Text>
+      </VStack>
+    </Container>
   )
 }
 
